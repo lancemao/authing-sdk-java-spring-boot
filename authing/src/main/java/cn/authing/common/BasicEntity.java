9ebf6b68d2f771fce507d6fe7e21f39a71fdd72f
liyunfei <chho93@163.com>
2021-08-19 16:00:50 +0800

    删掉注释

--- authing/src/main/java/cn/authing/common/BasicEntity.java
+++ authing/src/main/java/cn/authing/common/BasicEntity.java
@@ -4,12 +4,6 @@ import com.alibaba.fastjson.JSON;
 
 import java.io.Serializable;
 
-/**
- * BasicEntity
- *
- * @author chho
- * @date 2021/08/18
- */
 public class BasicEntity implements Serializable {
 
     @Override
